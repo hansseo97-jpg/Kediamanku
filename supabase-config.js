04b051482fa352fdaae651db78a4d2022aee42a0
hansseo97-jpg <hans.seo97@gmail.com>
2026-06-08 12:49:41 +0700

    Add supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+window.KEDIAMANKU_SUPABASE = {
+  projectId: "dzxbbpytiyemawwxthqr",
+  region: "ap-northeast-1",
+  url: "https://dzxbbpytiyemawwxthqr.supabase.co",
+  restUrl: "https://dzxbbpytiyemawwxthqr.supabase.co/rest/v1",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6eGJicHl0aXllbWF3d3h0aHFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzODI4MDQsImV4cCI6MjA5NTk1ODgwNH0.Ar_r4DJiO2zbB7yqPnZ-owq8RNNvs8X8Ygn-dG2bxV4",
+};
